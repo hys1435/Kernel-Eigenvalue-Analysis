00c3dd3ae73c85a30e9a47b72e7f817d27a04f35
Zhaoqi Li <hys1435@users.noreply.github.com>
2019-11-07 10:27:57 -0800

    Add files via upload

---
+++ KRR_algorithm_copy.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 22 21:48:30 2019
+
+@author: Zhaoqi Li
+"""
+
+# Kernel Ridge Regression Algorithm reproducing results from zhang15d paper
+
+import numpy as np
+
+def gaussianRBF(u, v, params):
+    sigma = params[0]
+    if (len((u-v).shape) == 1): 
+        return np.exp(-np.einsum('i,i',u-v,u-v)/(2*sigma**2))
+    return np.exp(-np.einsum('ij,ij->i',u-v,u-v)/(2*sigma**2)) # einsum computes norm squared
+
+def sobolev(u, v, params):
+    return 1 + np.minimum(u, v)
+    
+def get_kernel(dist_metric):
+    if (dist_metric == "gaussian"):
+        return gaussianRBF
+    elif (dist_metric == "sobolev"):
+        return sobolev
+    
+def f_star(x):
+    return np.minimum(x, 1-x)
+
+def compute_gram_mat(X1, X2, params, dist_metric):
+    kernel = get_kernel(dist_metric)
+    gram_mat = np.zeros((X1.shape[0],X2.shape[0]))
+    for i, item in enumerate(X2):
+        gram_mat[:,i] = kernel(X1, item, params)
+    return gram_mat
+
+def compute_kernel_ridge_coeffs(X, y, params, dist_metric):
+    # Key algorithm to compute the kernel ridge coefficients alpha
+    K = compute_gram_mat(X, X, params, dist_metric)
+    alpha = compute_coeffs_from_K(K, y, params)
+    return alpha
+
+def compute_coeffs_from_K(K, y, params):
+    # compute the kernel coefficients alpha given the gram matrix K, formula listed as 
+    # equantion (37) of the original paper
+    lam = params[-1]
+    alpha = np.linalg.solve(K + lam * y.size * np.eye(K.shape[0]), y)
+    return alpha
+
+def split_into_m_parts(X, m):
+    # split the entire dataset into subsets
+    n = int(X.shape[0] / m)
+    resShape = [m, n]
+    if len(X.shape) > 1:
+        for item in X.shape[1:]:
+            resShape.append(item)
+    res = np.zeros(resShape)
+    for i in range(m):
+        res[i] = X[i*n:(i+1)*n]
+    return res
+    
+def predict(X_train, X_test, alpha, m, params, dist_metric, output = False):
+    # compute the prediction of y using kernel coefficients alpha
+    K = compute_gram_mat(X_test, X_train, params, dist_metric)
+    #print("K: ", K.shape)
+    #print("alpha: ", alpha.shape)
+    y_pred = np.dot(K, alpha)
+    if (output):
+        return y_pred, K, alpha
+    return y_pred
+
+def compute_mse(X, y, N, m, params, dist_metric, 
+                X_test = None, y_test = None, real = False, integral = False):
+    # Key function to compute the mse, real is the parameter indicating if it's 
+    # simulation study or real data
+    # n = int(N / m)
+    if (real): 
+        y_pred_lst = np.zeros((m, X_test.shape[0]))
+    elif (integral):
+        y_pred_lst = np.zeros((m, 200))
+    else:
+        y_pred_lst = np.zeros((m, N))
+    X_split = split_into_m_parts(X, m)
+    y_split = split_into_m_parts(y, m)
+    for k, (XX, yy) in enumerate(zip(X_split, y_split)):
+        alpha = compute_kernel_ridge_coeffs(XX, yy, params, dist_metric)
+        if (real):
+            y_pred_lst[k] = predict(XX, X_test, alpha, m, params, dist_metric)
+        elif (integral): 
+            X_seq = np.linspace(start = 1e-4, stop = 1, num = 200)
+            y_pred_lst[k] = predict(XX, X_seq, alpha, m, params, dist_metric)
+        else:
+            y_pred_lst[k] = predict(XX, X, alpha, m, params, dist_metric)
+    if (integral):
+        y_test = f_star(X_seq)
+    elif (not real):
+        y_test = f_star(X)
+    y_pred = np.mean(y_pred_lst, axis = 0)
+    mse = np.mean((y_test - y_pred)**2)
+    return mse
+
+def compute_mse_no_avg(X, y, N, m, params, dist_metric, 
+                X_test = None, y_test = None, real = False):
+    # Key function to compute the mse, real is the parameter indicating if it's 
+    # simulation study or real data
+    n = int(N / m)
+    X1 = X[0:n]
+    y1 = y[0:n]
+    alpha = compute_kernel_ridge_coeffs(X1, y1, params, dist_metric)
+    y_pred = predict(X1, X_test, alpha, m, params, dist_metric) # multiply by m to remove averaging
+    mse = np.mean((y_test - y_pred)**2)
+    return mse
+    
+"""
+def compute_mse(X, y, N, m, p, params, dist_metric, 
+                X_test = None, y_test = None, real = False):
+    alpha = np.zeros(N)
+    n = int(N / m)
+    X_split = split_into_m_parts(X, m)
+    y_split = split_into_m_parts(y, m)
+    results = [p.apply_async(compute_kernel_ridge_coeffs, [XX, yy, params, dist_metric]) 
+                for XX, yy in zip(X_split, y_split)]
+    for k, r in enumerate(results):
+        alpha[k*n:(k+1)*n] = r.get()
+    if (real):
+        y_pred = predict(X, X_test, alpha, m, params, dist_metric)
+        mse = np.mean((y_test - y_pred)**2)
+    else:
+        y_pred = predict(X, X, alpha, m, params, dist_metric)
+        mse = np.mean((y - y_pred)**2)
+    return mse
+    """
---
+++ kpm_mnist.py
@@ -0,0 +1,135 @@
+"""
+=====================================================
+MNIST classfification using multinomial logistic + L1
+=====================================================
+
+Here we fit a multinomial logistic regression with L1 penalty on a subset of
+the MNIST digits classification task. We use the SAGA algorithm for this
+purpose: this a solver that is fast when the number of samples is significantly
+larger than the number of features and is able to finely optimize non-smooth
+objective functions which is the case with the l1-penalty. Test accuracy
+reaches > 0.8, while weight vectors remains *sparse* and therefore more easily
+*interpretable*.
+
+Note that this accuracy of this l1-penalized linear model is significantly
+below what can be reached by an l2-penalized linear model or a non-linear
+multi-layer perceptron model on this dataset.
+
+"""
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.datasets import fetch_openml
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils import check_random_state
+from kpm import select_D, gaussianRBF, compute_gram_mat, kpca, compute_coeff_fixD
+from KRR_algorithm_copy import compute_kernel_ridge_coeffs, predict
+from sklearn.kernel_ridge import KernelRidge
+
+
+print(__doc__)
+
+# Author: Arthur Mensch <arthur.mensch@m4x.org>
+# License: BSD 3 clause
+
+# Turn down for faster convergence
+t0 = time.time()
+train_samples = 1000
+test_samples=10000
+
+# Load data from https://www.openml.org/d/554
+X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+
+random_state = check_random_state(0)
+permutation = random_state.permutation(X.shape[0])
+X = X[permutation]
+y = y[permutation]
+X = X.reshape((X.shape[0], -1))
+
+y = y.astype(dtype=int)
+#print("data: ", X[0:10])
+#print("label: ", y[0:10])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, train_size=train_samples, test_size=test_samples)
+
+#print("shape of X: ", X_train.shape)
+#print("number of nonzero elements in X: ", np.count_nonzero(X_train))
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+#print("shape of X: ", X_train.shape)
+#print("number of nonzero elements in X: ", np.count_nonzero(X_train))
+
+# Turn up tolerance for faster convergence
+clf = LogisticRegression(C=50. / train_samples, solver='saga', tol = 0.01) # default l2 penalty
+clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+y_pred = clf.predict(X_test)
+print("y_pred: ", y_pred)
+mse_lr = np.mean((y_test - y_pred)**2)
+
+# Self-written Kernel Ridge Regression
+sigma = 20
+lam = 1/train_samples
+K = compute_gram_mat(X_train, X_train, gaussianRBF, sigma)
+alpha = compute_kernel_ridge_coeffs(X_train, y_train, [sigma, lam], "gaussian")
+y_pred = predict(X_train, X_test, alpha, 1, [sigma], "gaussian") # m = 1
+mse_krr_self = np.mean((y_test - y_pred)**2)
+
+"""
+# Kernel Ridge Regression
+clf2 = KernelRidge(kernel = 'rbf', gamma = 1/(2*sigma**2))
+clf2.fit(X_train, y_train)
+# score2 = clf2.score(X_test, y_test)
+y_pred = clf2.predict(X_test)
+mse_krr = np.mean((y_test - y_pred)**2)
+"""
+
+
+# Kernel Projection Machine
+C = 0.5
+D_max = 50
+print("K: ", K[0:10])
+print("shape of K: ", K.shape)
+print("number of nonzero elements in K: ", np.count_nonzero(K))
+
+D_opt, mse_kpm_train = select_D(K, y_train, D_max, C)
+print("optimal D: ", D_opt)
+K_test = compute_gram_mat(X_test, X_train, gaussianRBF, sigma)
+Alpha, Lam = kpca(K, D_opt)
+print("Shape of Alpha: ", Alpha.shape)
+Phi_test = K_test @ Alpha / np.sqrt(train_samples)
+print("Shape of Phi_test: ", Phi_test.shape)
+coeffs_opt, _ = compute_coeff_fixD(K, y_train, D_opt)
+print("Shape of coeffs_opt: ", coeffs_opt.shape)
+y_pred = Phi_test @ coeffs_opt
+mse_kpm_test = np.mean((y_test - y_pred)**2)
+
+
+print("Test score logistic regression with L2 penalty: %.4f" % score)
+print("Mean-squared error for logistic regression: %.4f" % mse_lr)
+#print("Mean-squared error for kernel ridge regression: %.4f" % mse_krr)
+print("Mean-squared error for kernel ridge regression self: %.4f" % mse_krr_self)
+print("Mean-squared error for kernel projection machine: %.4f" % mse_kpm_test)
+
+coef = clf.coef_.copy()
+plt.figure(figsize=(10, 5))
+scale = np.abs(coef).max()
+for i in range(10):
+    l1_plot = plt.subplot(2, 5, i + 1)
+    l1_plot.imshow(coef[i].reshape(28, 28), interpolation='nearest',
+                   cmap=plt.cm.RdBu, vmin=-scale, vmax=scale)
+    l1_plot.set_xticks(())
+    l1_plot.set_yticks(())
+    l1_plot.set_xlabel('Class %i' % i)
+plt.suptitle('Classification vector for...')
+
+run_time = time.time() - t0
+print('Example run in %.3f s' % run_time)
+plt.show()
